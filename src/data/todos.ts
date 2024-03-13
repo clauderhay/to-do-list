2e80c8423ff73d8c310d85857b43a8d07c29133b
Claude Rhay <claude.rhay@gmail.com>
2024-03-13 22:34:24 +0800

    P1-todo-list-complete-code
    A basic CRUD application using React.

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+    {
+        id: 1,
+        title: "Like my page 👍🏻",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "Follow me in all my socials 🍻",
+        completed: false,
+    },
+    {
+        id: 3,
+        title: "Subscribe for email updates 📩",
+        completed: false,
+    },
+]
